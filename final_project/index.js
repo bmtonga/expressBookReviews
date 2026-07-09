47859cb4df6483909bde894a0e27f8f8753059cb
bmtonga <mtongabernard6@gmail.com>
2026-07-09 11:09:44 +0200

    session authentication feacture implemented

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,14 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token = req.headers['authorization'].split(' ')[1];
+    jwt.verify(token, 'access', (err, user) => {
+        if(err){
+            return res.status(403).json({message: "Invalid token"});
+        }
+        req.user = user;
+        next();
+    });
 });
  
 const PORT =5000;
