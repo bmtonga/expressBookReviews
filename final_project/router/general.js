86fd4a40642100570a27f27f7c2995eda1a8b423
bmtonga <mtongabernard6@gmail.com>
2026-07-09 11:30:19 +0200

    Implement author search functionality in book details endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,10 +35,28 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
 
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const authorQuery = req.params.author;
+
+  if (!authorQuery) {
+    return res.status(400).json({ message: 'Invalid author' });
+  }
+
+  const results = Object.keys(books)
+    .map((k) => Number(k))
+    .filter((isbn) => {
+      const book = books[isbn];
+      return book && String(book.author).toLowerCase() === String(authorQuery).toLowerCase();
+    })
+    .map((isbn) => ({ isbn, ...books[isbn] }));
+
+  if (results.length === 0) {
+    return res.status(404).json({ message: 'Book not found' });
+  }
+
+  return res.status(200).json(results);
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
