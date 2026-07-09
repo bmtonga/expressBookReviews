789fd2fb386811be788030d282b70e76c48504c3
bmtonga <mtongabernard6@gmail.com>
2026-07-09 11:28:07 +0200

    Implement ISBN lookup functionality in book details endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,11 +18,22 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbnParam = req.params.isbn;
+  const isbn = Number(isbnParam);
+
+  if (!Number.isInteger(isbn)) {
+    return res.status(400).json({ message: 'Invalid ISBN' });
+  }
+
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: 'Book not found' });
+  }
+
+  return res.status(200).json({ isbn, ...book });
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
